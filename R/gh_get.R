6e40c53b277a9a2f427b1da59c150323c5dc769c
skaltman <skaltman@stanford.edu>
2020-01-02 16:47:16 -0800

    update

--- R/gh_get.R
+++ R/gh_get.R
@@ -19,3 +19,25 @@ gh_get <- function(repo_name, org, path) {
     gh_auth()
   )
 }
+
+#' Retrieve information about a repository
+#'
+#'
+#' @param repo_name Name of the repository
+#' @param org Name of the organization
+#'
+#' @return Response object
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' gh_get_repo("sara", "sara-org")
+#' }
+gh_get_repo <- function(repo_name, org) {
+  httr::GET(
+    stringr::str_glue(
+      "https://api.github.com/repos/{org}/{repo_name}"
+    ),
+    gh_auth()
+  )
+}
